a49a5e34b71af82f4ebae12cd44af3881a6d0e9c
Sonu Saha <ahasunos@gmail.com>
2025-02-26 22:05:23 +0530

    refactor: break down models from main.go
    Signed-off-by: Sonu Saha <ahasunos@gmail.com>

---
+++ backend/internals/models/profile.go
@@ -0,0 +1,26 @@
+package models
+
+import "time"
+
+// Profile struct to hold profile data
+type Profile struct {
+	ID          int       `json:"id"`
+	Name        string    `json:"name"`
+	URL         string    `json:"url"`
+	Description string    `json:"description"`
+	Stars       int       `json:"stars"`
+	LastUpdated time.Time `json:"last_updated"`
+}
+
+// GitHubSearchResult struct to parse GitHub API search response
+type GitHubSearchResult struct {
+	Items []GitHubRepo `json:"items"`
+}
+
+// GitHubRepo struct represents a GitHub repository
+type GitHubRepo struct {
+	Name        string `json:"name"`
+	HTMLURL     string `json:"html_url"`
+	Description string `json:"description"`
+	Stars       int    `json:"stargazers_count"`
+}
